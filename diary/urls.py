923f82dc891c7c54c95dbcb011e5c698440c1fb2
s1f102201672 <107388803+s1f102201672@users.noreply.github.com>
2024-11-11 14:02:53 +0900

    ダッシュボード削除

--- diary/urls.py
+++ diary/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('new/', views.create_diary_entry, name='create_entry'),
     path('<int:pk>/', views.diary_entry_detail, name='entry_detail'),
     path('', views.index, name='top_page'),
-    path('diary/',views.diary_list, name='diary_list'),# diary/ へのパス
-    path('sentiment-dashboard/', views.sentiment_dashboard, name='sentiment_dashboard'),  # 感情分析ダッシュボード
+    path('diary/', views.diary_list, name='diary_list'),# diary/ へのパス
 ]
