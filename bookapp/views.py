57fc8c9d3a27efc25b5c285d3ec9688630befb28
s1F102200132 <s1F102200132@iniad.org>
2024-11-14 13:58:02 +0900

    h

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,3 +1,24 @@
 # diary/views.py
 from django.shortcuts import render, redirect
- 
+from .models import DiaryEntry
+from .utils import analyze_sentiment, recommend_book_by_sentiment
+
+def create_diary_entry(request):
+    if request.method == "POST":
+        content = request.POST.get("content")
+        sentiment = analyze_sentiment(content)
+        recommended_book = recommend_book_by_sentiment(sentiment)
+        
+        diary_entry = DiaryEntry.objects.create(
+            user=request.user,
+            content=content,
+            sentiment=sentiment,
+            recommended_book=recommended_book
+        )
+        return redirect('diary:entry_detail', pk=diary_entry.pk)
+    
+    return render(request, 'diary/create_entry.html')
+
+def diary_entry_detail(request, pk):
+    diary_entry = DiaryEntry.objects.get(pk=pk)
+    return render(request, 'diary/entry_detail.html', {'diary_entry': diary_entry})
